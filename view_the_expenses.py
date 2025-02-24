6814ff791be140608fc9ca0e2785677ca8898ede
AlonsoOrtiz24 <aortizs.ieu2023@student.ie.edu>
2025-02-24 17:08:15 +0100

    Function to add expenses is done.

---
+++ view_the_expenses.py
@@ -0,0 +1,19 @@
+def view_expenses():
+    """
+    Displays all recorded expenses with dates
+    :return:
+    """
+    print("\nExpense Summary:")
+
+    total_spending = 0
+    for category, transactions in expenses.items():
+        print(f"\n{category}:")
+
+        if not transactions:
+            print("No expenses recorded.")
+        else:
+            for amount, date in transactions:
+                print(f"  - ${amount:.2f} on {date}")  # Shows amount and date
+                total_spending += amount
+    print(f"\nTotal Spending: ${total_spending:.2f}")
+
